f790ee2bb424c3d59029976551444a9cc98fcc0c
Sajjad Salaria <imsafay@gmail.com>
2017-10-15 11:47:27 +0530

    Create LabelMethod.py

---
+++ LabelMethod.py
@@ -0,0 +1,10 @@
+from tkinter import *
+myGui = Tk()
+myGui.title("Python-GUI")
+myGui.geometry("500x500+100+100")
+myLabel1 = Label(text='label one',fg='red').pack()
+myLabel2 = Label(text='label two',fg='green',bg='black')
+myLabel2 .pack()
+myGui.mainloop()
+ 
+ 
