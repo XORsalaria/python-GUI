35b7371bbbe0c2346d50578b1d78a0beacb8cd64
Sajjad Salaria <imsafay@gmail.com>
2017-10-15 11:49:55 +0530

    updated a comment

--- LabelMethod.py
+++ LabelMethod.py
@@ -2,7 +2,7 @@ from tkinter import *
 myGui = Tk()
 myGui.title("Python-GUI")
 myGui.geometry("500x500+100+100")
-myLabel1 = Label(text='label one',fg='red').pack()
+myLabel1 = Label(text='label one',fg='red').pack() # pack places the label in center
 myLabel2 = Label(text='label two',fg='green',bg='black')
 myLabel2 .pack()
 myGui.mainloop()
